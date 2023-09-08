34383e135b814d87e4a12760eb2ae8bc54c4fb1a
Shashank Tiwari <1928260@kiit.ac.in>
2023-09-08 17:26:40 +0530

    Second commit

--- src/main/java/com/programming/SpringBootConcept/SpringMVCBootJavaProject/ApplicationConfig.java
+++ src/main/java/com/programming/SpringBootConcept/SpringMVCBootJavaProject/ApplicationConfig.java
@@ -4,7 +4,10 @@ package com.programming.SpringBootConcept.SpringMVCBootJavaProject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -28,4 +31,18 @@ public class ApplicationConfig extends WebMvcConfigurationSupport {
     viewResolver.setViewClass(JstlView.class);
     return viewResolver;
 }
+    @Override
+    protected void configureAsyncSupport(AsyncSupportConfigurer configurer)
+    {
+        configurer.setDefaultTimeout(5000);
+        configurer.setTaskExecutor(mvcTaskExecutor());
+    }
+
+    @Bean
+    public AsyncTaskExecutor mvcTaskExecutor()
+    {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setThreadNamePrefix("hplus-thread-");
+        return threadPoolTaskExecutor;
+    }
 }
--- src/main/java/com/programming/SpringBootConcept/SpringMVCBootJavaProject/controllers/SearchController.java
+++ src/main/java/com/programming/SpringBootConcept/SpringMVCBootJavaProject/controllers/SearchController.java
@@ -2,6 +2,7 @@ package com.programming.SpringBootConcept.SpringMVCBootJavaProject.controllers;
 
 import com.programming.SpringBootConcept.SpringMVCBootJavaProject.beans.Product;
 import com.programming.SpringBootConcept.SpringMVCBootJavaProject.repository.ProductRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
 
 @Controller
 public class SearchController {
@@ -17,15 +19,30 @@ public class SearchController {
     @Autowired
     private ProductRepository productRepository;
 
-    @GetMapping("/search")
-    public String searchPage(@RequestParam("search") String search, Model model){
-        System.out.println("In Search Controller");
-        System.out.println("Searching for"+ search);
+    /* @GetMapping("/search")
+     public String searchPage(@RequestParam("search") String search, Model model){
+         System.out.println("In Search Controller");
+         System.out.println("Searching for"+ search);
 
-        List<Product> products = new ArrayList<>();
-        products =productRepository.searchByName(search);
-        model.addAttribute("products",products);
-        return "search";
+         List<Product> products = new ArrayList<>();
+         products =productRepository.searchByName(search);
+         model.addAttribute("products",products);
+         return "search";
+     }*/
+    @GetMapping("/search")
+    public Callable<String> searchPage(@RequestParam("search") String search, Model model, HttpServletRequest request) {
+        System.out.println("In Search Controller");
+        System.out.println("Searching for" + search);
+        System.out.println("Async Programming Supported in Spring Boot" + request.isAsyncSupported());
+        System.out.println("Thread Created by Servlet Container" + Thread.currentThread().getName());
+        return () -> {
+            Thread.sleep(3000);
+            System.out.println("Thread from Spring MVC Task Executor is" + Thread.currentThread().getName());
+            List<Product> products = new ArrayList<>();
+            products = productRepository.searchByName(search);
+            model.addAttribute("products", products);
+            return "search";
+        };
     }
 
 }
